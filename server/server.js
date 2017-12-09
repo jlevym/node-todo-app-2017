9ad5b43338d083b508680b5320e669c86b360ea0
jlevym <jlevym@gmail.com>
2017-12-09 16:49:43 +0200

    implement post route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    "text": req.body.text
+  });
+
+ todo.save().then((doc) => {
+   res.status(200).send(doc);
+ }, (e) => {
+   res.send(e);
+ });
+});
+
+app.listen(4000, () => {
+  console.log('listening on port 4000');
+});
