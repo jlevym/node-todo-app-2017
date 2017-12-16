8cb1d2119eabbc4d96f03802c8e732384f029adb
jlevym <jlevym@gmail.com>
2017-12-16 14:56:42 +0200

    fix mongodbURl syntac

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/ToDoApp'});
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/ToDoApp');
 
 
 module.exports = {mongoose};
