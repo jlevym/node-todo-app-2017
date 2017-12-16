29a996d64c42189a220ac7b5fcfc194a58f045c2
jlevym <jlevym@gmail.com>
2017-12-16 15:59:09 +0200

    , {   useMongoClient: true

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp', {
+  useMongoClient: true
+ });
 
 
 module.exports = {mongoose};
