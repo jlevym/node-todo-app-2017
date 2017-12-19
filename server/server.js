8db22b051e18c5198fe9e007442f718d1e841ef6
jlevym <jlevym@gmail.com>
2017-12-19 18:36:45 +0200

    add PUSH /:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {ObjectID} = require('mongodb');
 
@@ -70,6 +71,30 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+// start PATCH todos section
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ["text", "completed"]);
+  console.log(body);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.competedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+
+    res.send({todo})
+  }).catch((e) => {res.status(400).send()
+    });
+});
 
 // starting users section
 app.post('/users', (req, res) => {
