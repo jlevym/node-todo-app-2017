421234cf82766f4c8bf44f3c14da216a35946175
jlevym <jlevym@gmail.com>
2017-12-22 09:19:51 +0200

    complete get/users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -114,17 +115,8 @@ app.post('/users', (req, res) => {
    })
 });
 
-
-app.get('/users/me', (req, res) => {
-  var token = req.header('x-auth');
-
-  User.findByToken(token).then((user) => {
-    if (!user) {
-
-    }
-
-    res.send(user);
-  });
+app.get('/users/me', authenticate,  (req, res) => {
+  res.send(req.user);
 });
 
 app.get('/users', (req, res) => {
