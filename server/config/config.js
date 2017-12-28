bd5db905c9051c5f08694b7c3bbfbc64e5c63119
jlevym <jlevym@gmail.com>
2017-12-28 17:37:15 +0200

    fix bug in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,8 @@ var env = process.env.NODE_ENV || 'development';
 if (env === 'development' || env === 'test') {
   var config = require('./conf.json');
   var envConfig = config[env];
-}
+
 Object.keys(envConfig).forEach((key) => {
   process.env[key] = envConfig[key];
-})
+  });
+}
